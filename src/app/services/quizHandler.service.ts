e59b243ded492a748dc748f4090e0153bce86e8e
lgcolella <luigicolella.1997@gmail.com>
2018-11-13 23:51:36 +0100

    style: Simplified getResult method of the quizHandler service

--- src/app/services/quizHandler.service.ts
+++ src/app/services/quizHandler.service.ts
@@ -59,61 +59,39 @@ export class QuizHandler {
     /**
      * Process the provided answers and get result according to loaded quiz object
      * @param {Array} answers an array of answer's values
-     * @returns {Object} the result object
+     * @returns {Object} the result object matching the provided answers
      */
     getResult(answers : AnswerValue[]) : Result {
-        
-        //Results matching with provided answers
+
         let answerObjs: Result[];
-        //Get result according to quiz type
-        switch (this.currentQuiz.settings.type){
+        if (this.currentQuiz.settings.type === PERSONALITY_QUIZ) {
 
-            case (PERSONALITY_QUIZ) :
-                //Create an object where store each answers' value with its frequency
-                let answersWithFrequency: {[key: string]: number} = answers.reduce((prevResult, currValue) => {
-                    if (typeof currValue !== 'string') throw `Valore di ${currValue} non valido.`;
-                    let result = prevResult.hasOwnProperty(currValue) ? prevResult[currValue] += 1 : 1;
-                    return Object.assign({}, prevResult, {
-                        [currValue]: result
-                    })
-                }, {});
-                
-                //Get answer with higher frequency
-                let answerWithHigherFrequency: string = 
-                    Object.entries(answersWithFrequency)
-                    .reduce(
-                        (prevResult, currValue) => (currValue[1] > prevResult[1] ? currValue : prevResult)
-                    )[0]
-                    .trim().toLowerCase();
+            // Sort answers values in order to group them
+            let groupedAnswers = answers.sort();
+            // Get answer with higher frequency comparing their offset in `groupedArray` array
+            let answerWithHigherFrequency = groupedAnswers.sort((a, b) => {
+                let aFrequency = groupedAnswers.lastIndexOf(a) - groupedAnswers.indexOf(a);
+                let bFrequency = groupedAnswers.lastIndexOf(b) - groupedAnswers.indexOf(b);
+                return bFrequency - aFrequency;
+            })[0]
+            //Return object matching with answerWithHigherFrequency
+            answerObjs = this.currentQuiz.answers.filter(answer => answer.value === answerWithHigherFrequency);
+        
+        } else if (this.currentQuiz.settings.type === TRUEORFALSE_QUIZ) {
 
-                //Return result object matching with answerWithHigherFrequency
-                answerObjs = this.currentQuiz.answers.filter(
-                    (obj) => obj.value.trim().toLowerCase() === answerWithHigherFrequency
-                );
-            break;
-            case (TRUEORFALSE_QUIZ) :
-                //Get number of correct answers
-                let correctAnswers = 0;
-                for (let i = 0; i < answers.length; i++){
-                    if (answers[i]) correctAnswers++
-                }
-                //Filter answer objects matching with number of correct answers
-                answerObjs = this.currentQuiz.answers.filter((answerObj) => {
-                    let answerValue = answerObj.value;
-                    let rangeSeparator = '-'; //If answerValue is a range then it has this separator
-                    //Detect if answerValue is a single number or a range
-                    if (answerValue.indexOf(rangeSeparator) !== -1){
-                        //Get numerical values of the range
-                        let maxValue = Math.max( ...answerValue.split(rangeSeparator).map(Number) );
-                        let minValue = Math.min( ...answerValue.split(rangeSeparator).map(Number) );
-                        //Return if correctAnswers matches with provided range
-                        return minValue <= correctAnswers && correctAnswers <= maxValue;
-                    } else {
-                        //Return if correctAnswers matches with answerObj value
-                        return correctAnswers === Number(answerValue); 
-                    }
-                })
-            break;
+            //Get number of correct answers
+            let correctAnswers = 0;
+            for (let i = 0; i < answers.length; i++){
+                if (answers[i]) correctAnswers++
+            }
+            // Filter answer objects matching with number of correct answers
+            answerObjs = this.currentQuiz.answers.filter((answer) => {
+                let rangeSeparator = '-'; // If `answer.value` is a range then it has this separator
+                // Return answer if is between max and min allowed values
+                let maxValue = Math.max( ...answer.value.split(rangeSeparator).map(Number) );
+                let minValue = Math.min( ...answer.value.split(rangeSeparator).map(Number) );
+                return minValue <= correctAnswers && correctAnswers <= maxValue;
+            })
 
         }
 
